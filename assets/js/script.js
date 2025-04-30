4dee7071b2026a49182e979fb1ae1bcd682ecbbb
wholeomarfaruk <wholeomarfaruk@gmail.com>
2025-05-01 02:49:00 +0600

    slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,17 @@ var TrandingSlider = new Swiper('.tranding-slider', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+     
+    },
+    576: {
+      slidesPerView: "auto",
+     
+    },
+
   }
 });
   
